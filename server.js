f85f0af1cf7a38c30c84aabaf438b3adaa81ba94
Kyle Wilkins <Kyle.Wilkins@bbd.co.za>
2025-01-21 09:04:31 +0200

    updated Install AWS CLI commnad

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ exports.handler = async (event) => {
     return {
         statusCode: 200,
         headers: {
-            'Content-Type': 'text/plain',
+            "Content-Type": "text/html"
         },
-        body: 'Hello, World!\n',
+        body: "<html><body><h1>Hello, World!</h1></body></html>"
     };
 };
